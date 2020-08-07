8323df91c3805d86ee9ecf886e5190c837c19cd9
Mateusz Adamowicz <mateusz.adamowicz.1994@gmail.com>
2020-08-07 12:03:25 +0200

    Version endpoint

---
+++ src/AlgorithmsAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlgorithmsAPI.Web.Controllers
+{
+    [ApiController]
+    [Route("/api/")]
+    public class BaseController : ControllerBase
+    {
+        [HttpGet("info")]
+        public ActionResult<string> Info()
+        {
+            Assembly assembly = typeof(Startup).Assembly;
+
+            DateTime creationDate = System.IO.File.GetCreationTime(assembly.Location);
+            string version = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+
+            return Ok($"Version: {version}, Last Updated: {creationDate}");
+        }
+    }
+}
